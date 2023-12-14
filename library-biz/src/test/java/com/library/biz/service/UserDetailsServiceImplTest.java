c4f332ead43087927453805177867233d66fc5c5
Lai Haimeng <1024303455@qq.com>
2023-12-14 09:31:02 +0800

    Add unit test

---
+++ library-biz/src/test/java/com/library/biz/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.library.biz.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @description:
+ * @author: Lai Haimeng
+ * @date: 2023/12/14
+ */
+class UserDetailsServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void loadUserByUsername() {
+
+    }
+}
