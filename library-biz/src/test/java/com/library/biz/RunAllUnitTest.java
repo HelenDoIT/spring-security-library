7c5444fd73bf9eeca73ff056eb2802f7eaf87f8b
Lai Haimeng <1024303455@qq.com>
2023-12-14 09:27:37 +0800

    Add Junit suit test

---
+++ library-biz/src/test/java/com/library/biz/RunAllUnitTest.java
@@ -0,0 +1,16 @@
+package com.library.biz;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+/**
+ * @description:
+ * @author: Lai Haimeng
+ * @date: 2023/12/14
+ */
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.library.biz")
+public class RunAllUnitTest {
+
+}
